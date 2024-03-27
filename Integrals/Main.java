ff8d232a6a50f630d8416a8197aa209f960b4978
unknown <lugab037@gmail.com>
2024-03-26 21:12:42 -0300

    Regra do trapézio adicionada.

--- Integrals/Main.java
+++ Integrals/Main.java
@@ -1,9 +1,61 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String args[]) {
-        RiemannSum.principal(155, 12);
+        Scanner scanner = new Scanner(System.in);
+
+        int[] variation = getVariation(2, 0);
+
+        int choose = 0;
+        do {
+            System.out.println("0. Parar | 1. Soma de Riemann | 2. Regra do Trapézio");
+            choose = scanner.nextInt();
+
+            int repeticoes;
+            switch (choose) {
+                case 1:
+                    System.out.println("Quantas repetições deseja utilizar? Recomenda-se mais do que 100, no mínimo.");
+                    System.out.print("Digite o valor: ");
+                    repeticoes = scanner.nextInt();
+
+                    if (repeticoes < 1) {
+                        repeticoes = 1;
+                    }
+
+                    RiemannSum.principal(variation[0], variation[1], repeticoes);
+                    break;
+                case 2:
+                    System.out.print("Digite o número de repetições (menor do que 2 para regra do trapézio simples): ");
+                    repeticoes = scanner.nextInt();
+
+                    if (repeticoes < 1) {
+                        repeticoes = 1;
+                    }
+
+                    TrapezoidalRule.principal(variation[0], variation[1], repeticoes);
+                    break;
+            }
+
+            System.out.println("");
+        }
+        while (choose != 0);
+
+        scanner.close();
+        
     }
 
     public static double funcao(double x) {
-        return 4*x*x*x+7;
+        return Math.sqrt(x);
+    }
+
+    public static int[] getVariation(int a, int b) {
+        if (a > b) {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
+        int[] vetor = {a, b};
+        return vetor;
     }
 }
--- Integrals/TrapezoidalRule.java
+++ Integrals/TrapezoidalRule.java
@@ -1,26 +1,27 @@
 import java.text.DecimalFormat;
 
-class RiemannSum {
+public class TrapezoidalRule {
     public static void principal(double a, double b, int numPartitions) {
-        DecimalFormat formato = new DecimalFormat("#.##");
-
         double xDelta = (b-a)/numPartitions;
         double sum = 0;
 
         for (int i = 0, j = 0; i < numPartitions; i++, j++) {
             double pointA = a + i*xDelta;
             double pointB = a + (i+1)*xDelta;
-            double height = Main.funcao(pointA);
+
+            double sub = pointB - pointA;
+            double sumF = Main.funcao(pointA) + Main.funcao(pointB);
 
             if (i == 0 || j == numPartitions/10) {
                 String partition = "("+pointA+", "+pointB+")";
-                System.out.println("Partition "+(i)+": "+partition+" / "+height);
+                System.out.println("Partition "+(i)+": "+partition);
                 j = 0;
             }
 
-            sum += xDelta*height;
+            sum += (sub*sumF)/2;
         }
 
+        DecimalFormat formato = new DecimalFormat("#.##");
         System.out.println("\nIntegral: "+formato.format(sum));
     }
 }
